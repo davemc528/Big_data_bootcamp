e9eeb65b091302961b350e1b32f65a2c1336c8d0
davemc528 <dcunnin1@tulane.edu>
2017-05-19 15:12:37 -0500

    Lesson completion key for lesson 2 (vectors/factors) added.

--- lessonKey01.R
+++ lessonKey01.R
@@ -1,2 +1,4 @@
 write("Lesson01_DJYSX",file = "~/BigTapSwirl/lessonKeys.txt")
 #write the lesson key to verify completion as a subdirectory of the User name 
+write("Lesson02_RGKPS",file = "~/BigTapSwirl/lessonKeys.txt",append = TRUE)
+#key for lesson 2
